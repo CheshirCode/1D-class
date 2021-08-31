c6b8e46e6933154dbd988b7a61b1e88f3ef82e02
CheshireCode <50378606+CheshirCode@users.noreply.github.com>
2021-08-31 07:31:28 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const menuButton = document.querySelector(".header__burger");
+const menuButton1 = document.querySelector(".header__menu");
+const contentBody = document.querySelector("body");
+
+menuButton.onclick = function () {
+  menuButton.classList.toggle("active");
+  menuButton1.classList.toggle("active");
+  contentBody.classList.toggle("lock");
+}
+menuButton1.onclick = function () {
+  menuButton.classList.remove("active");
+  menuButton1.classList.remove("active");
+  contentBody.classList.remove("lock");
+}
